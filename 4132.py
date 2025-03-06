e1c1bcb80b319f4105be279449a853485462a87e
TiP0ks <67590401+TiP0ks@users.noreply.github.com>
2025-03-07 00:55:08 +0300

    Update 4132.py

--- 4132.py
+++ 4132.py
@@ -3,19 +3,19 @@ v, k, n = map(int, f.readline().split())
 a = [v]*k
 data = [int(x) for x in f]
 data.sort(reverse=True)
-print(data)
 c=0
 local_finder=[]
+f=0
 for i in range(n):
-    f=0
+    if f>7:
+        f-=k
     for j in range(f,k):
         if data[i] <= a[j]:
             a[j]-=data[i]
+            f += 1
             break
-            f+=1
         else:
             continue
     else:
         local_finder.append(data[i])
 print(sum(local_finder),len(local_finder))
-print(local_finder)
